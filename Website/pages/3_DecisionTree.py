757a2a1dd9e8dc671a358a3a3c460c3a2d9f31de
engrobelf <f.m.g.leborgne@tue.nl>
2023-04-05 14:26:41 +0200

    Update 3_DecisionTree.py

--- Website/pages/3_DecisionTree.py
+++ Website/pages/3_DecisionTree.py
@@ -51,8 +51,6 @@ prediction1, prediction2, prediction3 =st.columns([1,2,1])
 title1, title2, title3 = st.columns([1,2,1])
 explanation1, explanation2, explanation3 = st.columns([2,3,1])
 footer1, footer2, footer3 =st.columns([1,2,1])
-evaluation1, evaluation2, evaluation3 = st.columns([1,2,1])
-presentation1, presentation2, presentation3 = st.columns([2,2,2])
 
 
 @st.cache_data(persist=True)
@@ -193,16 +191,6 @@ with explanation1:
     st.dataframe(st.session_state.title_df.set_index('Title indices'))
     st.dataframe(st.session_state.gender_df.set_index('Gender indices'))
     st.dataframe(st.session_state.ports_df.set_index('Ports indices'))
-
-with presentation1: 
-    st.dataframe(st.session_state.ports_df.set_index('Ports indices'))
-
-with presentation2: 
-    st.dataframe(st.session_state.title_df.set_index('Title indices'))
-
-with presentation3: 
-    st.dataframe(st.session_state.gender_df.set_index('Gender indices'))
-
 
 with footer2:
     if (st.session_state.index2 < len(st.session_state.profileIndices)-1):
