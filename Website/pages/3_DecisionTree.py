5e925885d4c94b869b286af13fe5cbbc51545732
engrobelf <f.m.g.leborgne@tue.nl>
2023-04-05 14:24:07 +0200

    Update 3_DecisionTree.py

--- Website/pages/3_DecisionTree.py
+++ Website/pages/3_DecisionTree.py
@@ -49,7 +49,7 @@ header1, header2, header3 = st.columns([1,2,1])
 characteristics1, characteristics2, characteristics3 = st.columns([1,8,1])
 prediction1, prediction2, prediction3 =st.columns([1,2,1])
 title1, title2, title3 = st.columns([1,2,1])
-explanation1, explanation2, explanation3 = st.columns([1,2,1])
+explanation1, explanation2, explanation3 = st.columns([2,3,1])
 footer1, footer2, footer3 =st.columns([1,2,1])
 evaluation1, evaluation2, evaluation3 = st.columns([1,2,1])
 presentation1, presentation2, presentation3 = st.columns([2,2,2])
@@ -146,7 +146,7 @@ with prediction2:
     else:
         prob = round((probability[0][1]*100),2)
         st.markdown("The model predicts with {}% probability  that {}  will :green[**survive**]".format(prob, name) )
-        
+
 with title2: 
     st.subheader("Visualization - Decision Tree")
 
