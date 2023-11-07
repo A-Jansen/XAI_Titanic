a3802f002ee80e3189ee94f388fbde7972086306
A-Jansen <anniekjee@hotmail.com>
2023-11-07 08:23:03 +0100

    Update 5_visualMap.py

--- Website/pages/5_visualMap.py
+++ Website/pages/5_visualMap.py
@@ -127,7 +127,7 @@ with characteristics2:
 with explanation2:
 
     # st.write("Click on the image to see the shap values")
-    components.iframe("https://observablehq.com/embed/d177ef99668b6553@1222?cells=name%2Cimg%2Cpredictoin%2Cchart2%2Cviewof+button", scrolling=False, height=954)
+    components.iframe("https://observablehq.com/embed/d177ef99668b6553@1289?cells=name%2Cimg%2Cpredictoin%2Cinstruction%2Cchart2%2Cviewof+button", scrolling=False, height=954)
 
 
 
